2cf13c5b2d6035b694bef728cde94c2367e554b8
Jan Bak <jan.pawel.bak01@gmail.com>
2021-03-18 10:31:26 +0100

    First task to class

--- lab1/index.js
+++ lab1/index.js
@@ -1,2 +1,15 @@
 var userName = 'John';
 document.body.innerHTML = "<h1>Hello " + userName + "</h1>";
+var Person = /** @class */ (function () {
+    function Person(name, surname, age) {
+        this._name = name;
+        this._surname = surname;
+        this._age = age;
+    }
+    Person.prototype.Show = function () {
+        document.body.innerHTML = "<h1>Hello " + this._name + " " + this._surname + ", you are " + this._age + " yo</h1>";
+    };
+    return Person;
+}());
+var p = new Person("John", "Blake", 11);
+p.Show();
--- lab1/index.ts
+++ lab1/index.ts
@@ -2,3 +2,23 @@
 const userName: string = 'John';
 
 document.body.innerHTML = `<h1>Hello ${userName}</h1>`;
+
+class Person{
+    _name: string;
+    _surname: string;
+    _age: number;
+    constructor(name: string, surname: string, age: number)
+    {
+        this._name = name;
+        this._surname = surname;
+        this._age = age;
+    }
+    Show()
+    {
+        document.body.innerHTML = `<h1>Hello ${this._name} ${this._surname}, you are ${this._age} yo</h1>`;
+    }
+}
+
+let p = new Person("John", "Blake", 11);
+
+p.Show();
