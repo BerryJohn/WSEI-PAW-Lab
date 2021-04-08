d2d5c8399546c5e1d6e8980b44067e125baf35c6
Jan Bak <jan.pawel.bak01@gmail.com>
2021-04-08 12:42:00 +0200

    back to last commit xD

--- lab3/src/index.ts
+++ lab3/src/index.ts
@@ -4,17 +4,24 @@ import { gameFactory } from "./gameFactory.class";
 
 class App {
     private _gameFactory: gameFactory;
+    private gameContainer: HTMLDivElement;
 
     constructor() {
         this._gameFactory = new gameFactory();
-        this.initMenu();     
+        this.initMenu();        
+    }
+
+    changeGame(name: string): void{
+        const game = this._gameFactory.createGame(Games[name]);
+        this.gameContainer.innerHTML = ''; //clear
+        this.gameContainer.appendChild(game.getGameElement());
     }
 
     initMenu(): void {
         const menuContainer = <HTMLDivElement>(document.createElement('div')); // kontener menu dostępnych gier
-        const gameContainer = <HTMLDivElement>(document.createElement('div')); // kontener główny ekranu z grą
+        this.gameContainer = <HTMLDivElement>(document.createElement('div')); // kontener główny ekranu z grą
         const list = document.createElement('ul'); // lista pozycji w menu dostępnych gier
-        
+
         // TODO: Zaimplementuj wzorzec fabryki/metody fabrykującej, tak aby na podstawie konkretnej wartości z enum
         // zwrócić obiekt gry. Z tego obiektu można następnie pobrać nazwę gry i dodać do menu oraz metodę zwracającą
         // samą grę i po kliknięciu w wybrany element listy wywoływać ją, aby doklejać zawartość do gameContainer.
@@ -26,16 +33,17 @@ class App {
                     const gameName = <HTMLLIElement>(document.createElement('li'));
                     gameName.innerText = `${element}`;
                     gameName.addEventListener('click', e => {
-                        const selectedGame: string = (e.target as HTMLElement).innerText;
-                        this._gameFactory.createGame(Games[selectedGame])
+                        const newGameName: string = (e.target as HTMLElement).innerText;
+                        this.changeGame( newGameName )
                     });
                     list.appendChild(gameName);
                 }
         }
+
         menuContainer.appendChild(list);
         document.body.appendChild(menuContainer);
-        document.body.appendChild(gameContainer);
+        document.body.appendChild(this.gameContainer);
     }
 }
 
-new App();
+new App(); 
