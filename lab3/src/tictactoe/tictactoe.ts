1b12eee76ba5e0ca0d707baa2082fae99ee29a73
Jan Bak <jan.pawel.bak01@gmail.com>
2021-06-09 17:49:15 +0200

    Saving last moves

--- lab3/src/tictactoe/tictactoe.ts
+++ lab3/src/tictactoe/tictactoe.ts
@@ -1,12 +1,12 @@
 import { Game } from "../game.model";
-import { board } from '../../../lab2b-ticktoe/src/Classes/board';
+// import { board } from '../../../lab2b-ticktoe/src/Classes/board';
+import { board } from './src_tictactoe/Classes/board';
 import { disabled, enabled, watch } from "../decorators";
 
 @enabled
 export class TicTacToe implements Game {
     name: string;
     isDisabled: boolean;
-
     constructor() {
         this.name = "Kółko i krzyżyk";
     }
@@ -22,7 +22,11 @@ export class TicTacToe implements Game {
             div.classList.add('board');
             const game: board = new board(3,div);
             game.init();
-            return div;  
+            const buttonBack = document.createElement('button');
+            buttonBack.innerHTML = 'Undo last move';
+            buttonBack.addEventListener('click',()=>{game.undoMove()})
+            div.appendChild(buttonBack);
+            return div;
         }
     }
 }
